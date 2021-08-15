05b078aa27b5f76b00c8be6cf3f6945ef49733a6
Deku <zachdsamson@gmail.com>
2021-08-15 14:36:59 -0700

    completed question array

--- README.md
+++
@@ -1,3 +0,0 @@
-# Professional README Generator Starter Code
-
-[How to create a Professional README](./readme-guide.md)
--- index.js
+++ index.js
@@ -6,21 +6,105 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name? (Required)',
+        name: 'username',
+        message: 'What is your github username? (Required)',
         validate: nameInput => {
           if (nameInput) {
             return true;
           } else {
-            console.log('Please enter your name!');
+            console.log('Please enter your github name!');
             return false;
           }
         }
-      }
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email adress? (Required)',
+    validate: email => {
+        if (email) {
+        return true;
+        } else {
+        console.log('Please enter your email adress!');
+        return false;
+        }
+    }
+    },
+    {
+    type: 'input',
+    name: 'projName',
+    message: 'What is the name of your project? (Required)',
+    validate: title => {
+        if (title) {
+        return true;
+        } else {
+        console.log('Please enter the title of your project!');
+        return false;
+        }
+    }
+    },
+    {
+    type: 'input',
+    name: 'projDesc',
+    message: 'Please write a short description of your project. (Required)',
+    validate: desc => {
+        if (desc) {
+        return true;
+        } else {
+        console.log('Please describe your project!');
+        return false;
+        }
+    }
+    },
+    {
+    type: 'checkbox',
+    name: 'license',
+    message: 'What kind of license do you want for this project? (Check one)',
+    choices: ['MIT', 'APACHE'],
+    validate: licen => {
+        if (licen.length > 0) {
+        return true;
+        } else {
+        console.log('Please select a license!');
+        return false;
+        }
+    }
+    },
+    {
+    type: 'input',
+    name: 'dependency',
+    message: 'What command should be run to install dependencies? (Required)',
+    validate: depInput => {
+        if (depInput) {
+        return true;
+        } else {
+        console.log('Please provide the command to install dependencies!');
+        return false;
+        }
+    }
+    },
+    {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'repoInstruction',
+        message: 'What does the user need to know about using the repo?',
+    },
+    {
+        type: 'input',
+        name: 'contributeInstruction',
+        message: 'What does the user need to know about contributing?',
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {
